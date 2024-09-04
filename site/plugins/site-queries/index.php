972e5624680c31af0e9a792dd5190539734e9ca9
Pro-Use <robertprouse@gmail.com>
2024-09-04 12:28:51 +0100

    Add site queries

---
+++ site/plugins/site-queries/index.php
@@ -0,0 +1,25 @@
+<?php
+
+Kirby::plugin('10pm/dawes-site-queries', [
+    'api' => [
+        'routes' => function ($kirby) {
+            return [
+              [
+                'pattern' => 'site-query',
+                'action'  => function () use ($kirby) {
+                    $cache_exists = false;
+                    $result = [
+                        "url"=> $site->url(),
+                        "title" => $site->meta_title(),
+                        "meta_description" => $site->meta_description(),
+                        "news_on" => $site->news_on()->toBool(),
+                        "news_text" => $site->news_text()->upper(),
+                    ];
+
+                    return new Response(json_encode($result), 'application/json', 200, ['X-Cached' => $cache_exists]);
+                }
+              ]
+            ];
+        }
+    ]
+]);
