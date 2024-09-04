ada5732759a70e756f806b7c9af927b30f567ac6
Pro-Use <robertprouse@gmail.com>
2024-09-04 12:50:33 +0100

    add contacts

--- site/plugins/site-queries/index.php
+++ site/plugins/site-queries/index.php
@@ -10,12 +10,24 @@ Kirby::plugin('10pm/dawes-site-queries', [
                 'action'  => function () use ($kirby) {
                     $cache_exists = false;
                     $result = [
-                        "url"=> $kirby->site()->url(),
-                        "title" => $kirby->site()->meta_title(),
-                        "meta_description" => $kirby->site()->meta_description(),
-                        "news_on" => $kirby->site()->news_on()->toBool(),
-                        "news_text" => $kirby->site()->news_text()->upper(),
+                        "result" => [
+                            "url"=> $kirby->site()->url(),
+                            "title" => $kirby->site()->meta_title(),
+                            "meta_description" => $kirby->site()->meta_description(),
+                            "news_on" => $kirby->site()->news_on()->toBool(),
+                            "news_text" => $kirby->site()->news_text()->upper(),
+                            "contacts" => []
+                         ]
                     ];
+
+                    $contacts = $kirby->site()->children()->find('contact');
+
+                    foreach($contacts as $contact){
+                        array_push($result['result'], [
+                            "contact" => $contact->contact(),
+                            "id" => $$contact->id()
+                        ]);
+                    }
 
                     return new Response(json_encode($result), 'application/json', 200, ['X-Cached' => $cache_exists]);
                 }
